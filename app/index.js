f15c4ac05f4fe619a7e118b0cf0dadd04e88df16
Mauricio Reis <mauricioreiscds@gmail.com>
2022-05-29 23:30:44 -0300

    feat: html and css of the finished form

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
-import "./index.css";
+import sheet from "./index.css" assert { type: "css" };
 import button from "./scripts/button.js";
 import exemplo from "./scripts/exemplo.js";
 
 button();
 exemplo();
+
+document.adoptedStyleSheets = [sheet];
+shadowRoot.adoptedStyleSheets = [sheet];
--- app/scripts/button.js
+++ app/scripts/button.js
@@ -1,5 +1,19 @@
 const button = () => {
-  console.log("button");
+  const $ = document.querySelector.bind(document);
+
+  const form = $("#form");
+  const sucess = $("#sucess");
+  const button = $("#btn");
+
+  // form.addEventListener(() => {});
+
+  button.addEventListener("click", () => {
+    if (true) {
+      form.style.display = "none";
+      sucess.style.display = "block";
+      console.log("logou");
+    }
+  });
 };
 
 export default button;
